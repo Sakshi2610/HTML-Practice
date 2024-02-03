dc8352876fc3a126ec16f2686e53b18baac538ee
Sakshi Rane <sakshiramchandrarane@gmail.com>
2024-02-03 21:50:24 +0530

    add new file

---
+++ JavascriptPractice/index.js
@@ -0,0 +1,30 @@
+// console.log(name)
+// var name = "sakshi"
+// console.log(name)
+// var name = "divya"
+// console.log(name)
+
+// let name = "sakshi"
+// console.log(name)
+// let name = "divya"
+// console.log(name)
+// name = "divya"
+// console.log(name)
+
+// const age = 22
+// console.log(age)
+// const age1 = 20
+// console.log(age1)
+// age = 22
+// console.log(age)
+
+// var var = 1
+// console.log(var)
+// let let = 2
+// console.log(let)
+// const const = 3
+// console.log(const)
+var name = "sakshi"
+console.log(typeof(name))
+var name = 123
+console.log(typeof(name))
